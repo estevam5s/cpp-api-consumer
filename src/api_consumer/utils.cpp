5e0e0ab24f2c33a6424e57f18f4c7d1911166cbd
estevam5s <estevamsouzalaureth@gmail.com>
2023-03-24 15:23:23 -0300

    🛠🧪 Implement utility functions for API requests in utils.cpp

---
+++ src/api_consumer/utils.cpp
@@ -0,0 +1,8 @@
+#include "utils.hpp"
+
+namespace utils {
+    std::string example_function(const std::string& input) {
+        // Implemente sua função utilitária aqui.
+        return input;
+    }
+}
